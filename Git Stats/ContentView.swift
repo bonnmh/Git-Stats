e812ce27c27986fd5ae6781c3acffb3eb9a60815
bonnmh <bonnmh@nami.trade>
2025-02-26 13:45:15 +0700

    Initial Commit

---
+++ Git Stats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Stats
+//
+//  Created by Brian on 26/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Stats/Git_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_StatsApp.swift
+//  Git Stats
+//
+//  Created by Brian on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Git_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
