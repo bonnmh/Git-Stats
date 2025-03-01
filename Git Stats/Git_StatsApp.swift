763dda2d23d3fc9a2f6e05f56e33552125e36a3f
bonnmh <bonnmh@nami.trade>
2025-03-01 17:20:27 +0700

    [Brian] update refresh time provider

--- Git Stats/Git_StatsApp.swift
+++ Git Stats/Git_StatsApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 @main
 struct Git_StatsApp: App {
+//    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             Mainview()
+//                .onChange(of: scenePhase) { oldValue, newValue in
+//                    if newValue == .background {
+//                        print("reloadAllTimelines")
+//                        WidgetCenter.shared.reloadAllTimelines()
+//                    }
+//                }
         }
     }
 }
